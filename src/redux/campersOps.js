f5b6b34ab000703836c17285db782bd01154e470
777Afanas <serhii2111@yahoo.com>
2026-07-18 11:37:00 +0300

    feat: implement fetchCampers and fetchCamperById async thunks

--- src/redux/campersOps.js
+++ src/redux/campersOps.js
@@ -13,9 +13,7 @@ export const fetchCampers = createAsyncThunk(
     try {
       // об'єкт фільтрів => params (Axios веретворить на ?location=Kyiv тощо)
       const response = await campersApi.get("/campers", { params: filters });
-
-      console.log("Лог зсередини танка:", response.data.items);
-      // Повертаємо масив items, який віддає бекенд
+      // Мок-пагінація віддає об'єкт { items: [...] }, повертаємо response.data.items
       return response.data.items;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -29,26 +27,10 @@ export const fetchCamperById = createAsyncThunk(
   async (id, thunkAPI) => {
     try {
       const response = await campersApi.get(`/campers/${id}`);
-
-      // Повертаємо дані одного конкретного кемпера (там об'єкт, а не масив items)
+      // Повертаємо дані одного конкретного кемпера (об'єкт, не масив items)
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
     }
   },
 );
-
-campersApi
-  .get("/campers")
-  .then((res) => {
-    console.log("Повна відповідь сервера:", res.data);
-    console.log("Чисті дані (масив):", res.data.items);
-  })
-  .catch((err) => console.error("Помилка запиту:", err.message));
-
-campersApi
-  .get("/campers/1")
-  .then((res) => {
-    console.log("Дані ОДНОГО кемпера з ID 1:", res.data);
-  })
-  .catch((err) => console.error("Помилка запиту ID:", err.message));
